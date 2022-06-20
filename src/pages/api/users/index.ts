d68bc46a7abf62937e34ccd2c37fd57a826e1dfb
paulo.vieira <paulo.vieira@acct.global>
2022-06-20 18:27:56 -0300

    implement new files

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,14 @@
+import {NextApiRequest, NextApiResponse} from "next"
+
+
+
+export default (request:NextApiRequest,response:NextApiResponse)=>{
+
+    const users =[
+        {id:1, name:'Paulo'},
+        {id:2, name:'Joao'},
+        {id:3, name:'Maria'},
+    ]
+
+    return response.json(users)
+}
